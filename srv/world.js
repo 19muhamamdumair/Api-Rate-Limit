8886f384ab8b4ffe2971f245af86a302f31b5bf7
Muhammad Umair <19muhammadumair@gmail.com>
2023-11-02 19:25:07 +0000

    added rate limit to requests

--- srv/world.js
+++ srv/world.js
@@ -1,4 +1,33 @@
-module.exports = (say)=>{
-    say.on ('hello', req => `Hello ${req.data.to}!`)
-  }
-  
+const Limiter = require('limiter').RateLimiter;
+
+module.exports = async function () {
+  const requestCounts = {};
+
+  this.on('READ', 'Customer', async (req, next) => {
+    try {
+      const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+      const key = `ratelimit:${clientIP}`;
+
+      if (!requestCounts[key]) {
+        requestCounts[key] = new Limiter({
+          tokensPerInterval: 3, // Maximum 3 requests per interval per IP
+          interval: 'minute', // The time interval for rate limiting
+        });
+      }
+
+      if (!requestCounts[key].tryRemoveTokens(1)) {
+        const error = new Error('Rate limit for your IP exceeded. Please try again later.');
+        error.status = 429;
+        throw error;
+      }
+
+      console.log("testing");
+      const customers = await next();
+
+      return customers;
+    } catch (error) {
+      throw error;
+    }
+  });
+};
